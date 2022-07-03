4d1fd1c01381cce7d6ff55b71fc682cceda4ba7e
Vyacheslav Semenenko <semenenkos@mail.ru>
2022-07-04 01:07:56 +0300

    Add Task

---
+++ Test_First_Quarter/Program.cs
@@ -0,0 +1,15 @@
+﻿void CreateNewlyFormedArray (string [] array)
+{
+     string[] ThreeCharactersArray = new string[array.Length];
+     for (int i = 0; i < array.Length; i++)
+     {      
+        if (array[i].Length <= 3)
+        {
+            ThreeCharactersArray[i] = array[i];
+            Console.WriteLine (ThreeCharactersArray[i]); 
+        }
+     }
+        Console.WriteLine (); 
+}
+string [] array = {"hello", "2", "world", ":-)"};
+CreateNewlyFormedArray (array);
